91d64b4f4f287d99524aeec3995627b474f8d332
Aakash Kc <aakashkc.x@gmail.com>
2018-07-19 20:03:30 +0100

    fixed arrow function

--- routes.js
+++ routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {res.render('index', {
-    title: 'Title',
-    body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-})});
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Title', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.' })
+});
 
 module.exports = router;
