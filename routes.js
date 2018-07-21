80d2f6c5847ad3f48e2129efb834edb7e4593632
Aakash Kc <aakashkc.x@gmail.com>
2018-07-21 18:08:11 +0100

    Formatting

--- routes.js
+++ routes.js
@@ -1,8 +1,13 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Title', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.' })
+    res.render('index', {
+        title: 'Title',
+        body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+    })
 });
 
 module.exports = router;
